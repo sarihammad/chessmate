983a758ee7daccea6aac7d366c916c5bf648a7b6
Sari Hammad <sari_hammad@hotmail.com>
2025-08-03 20:50:12 -0400

    test: add Spring Boot application integration tests

---
+++ server/src/test/java/com/devign/chessmate/ChessmateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devign.chessmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessmateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
