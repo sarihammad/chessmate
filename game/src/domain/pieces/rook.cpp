8a533575c4c9d573306b643e12fe1bfac3dbd925
Sari Hammad <sari_hammad@hotmail.com>
2025-08-03 20:44:59 -0400

    feat: implement Rook piece with straight movement

---
+++ game/src/domain/pieces/rook.cpp
@@ -0,0 +1,35 @@
+#include "domain/pieces/rook.hpp"
+#include "domain/board.hpp"
+
+namespace chess {
+
+bool Rook::isValidMove(const Position& from, const Position& to, const Board& board) const {
+    if (from.row != to.row && from.col != to.col) return false;
+
+    int rowDir = (to.row - from.row) != 0 ? (to.row - from.row) / std::abs(to.row - from.row) : 0;
+    int colDir = (to.col - from.col) != 0 ? (to.col - from.col) / std::abs(to.col - from.col) : 0;
+
+    Position check = from;
+    check.row += rowDir;
+    check.col += colDir;
+
+    while (check != to) {
+        if (!board.isEmpty(check)) return false;
+        check.row += rowDir;
+        check.col += colDir;
+    }
+
+    auto target = board.getPieceAt(to);
+    return !target || target->getColor() != color;
+}
+
+void Rook::markMoved() { 
+    hasMoved = true;
+}
+
+bool Rook::hasMovedAlready() const {
+    return hasMoved; 
+}
+
+
+}
