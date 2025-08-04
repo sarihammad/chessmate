8950d83feac48601b054620b8bb79c4ada420c9b
Sari Hammad <sari_hammad@hotmail.com>
2025-08-03 20:49:51 -0400

    feat: add WebSocket configuration for real-time communication

---
+++ server/src/main/java/com/devign/chessmate/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.devign.chessmate.config;
+
+import com.devign.chessmate.socket.GameSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final GameSocketHandler gameSocketHandler;
+
+    public WebSocketConfig(GameSocketHandler gameSocketHandler) {
+        this.gameSocketHandler = gameSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(gameSocketHandler, "/game").setAllowedOrigins("*");
+    }
+}
