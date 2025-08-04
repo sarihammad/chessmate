a465d468c9ffe0560c81e3acc6121c7dbae987ee
Sari Hammad <sari_hammad@hotmail.com>
2025-08-03 20:46:21 -0400

    feat: add Piece base class header with virtual interface

---
+++ game/include/domain/piece.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "utils/utils.hpp"
+#include "enums.hpp"
+#include <memory>
+
+namespace chess {
+
+class Board;
+
+class Piece {
+protected:
+    Color color;
+
+public:
+    explicit Piece(Color c) : color(c) {}
+    virtual ~Piece() = default;
+
+    Color getColor() const { return color; }
+    virtual PieceType getType() const = 0;
+
+    virtual bool isValidMove(const Position& from, const Position& to, const Board& board) const = 0;
+
+    virtual std::shared_ptr<Piece> clone() const = 0;
+
+    virtual char symbol() const = 0;
+};
+
+}
