9d32a0848940c1ba0b0171a9197d3a424a98e461
Sari Hammad <sari_hammad@hotmail.com>
2025-08-03 20:49:46 -0400

    feat: implement Spring Boot server application entry point

---
+++ server/src/main/java/com/devign/chessmate/ChessmateServerApplication.java
@@ -0,0 +1,13 @@
+package com.devign.chessmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessmateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessmateServerApplication.class, args);
+	}
+
+}
