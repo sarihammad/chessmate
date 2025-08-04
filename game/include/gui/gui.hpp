94676dbc2590d70e68031e5a5843ffa667616e5a
Sari Hammad <sari_hammad@hotmail.com>
2025-08-03 20:48:49 -0400

    feat: add GUI header with main interface

---
+++ game/include/gui/gui.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "infrastructure/network/websocket_client.hpp"
+
+namespace chess {
+    void runMainMenu(sf::RenderWindow& window, const std::string& serverUri);
+    void runGUI(sf::RenderWindow& window, WebSocketClient* wsClient = nullptr);
+}
